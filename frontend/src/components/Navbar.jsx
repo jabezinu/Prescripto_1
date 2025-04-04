0da5e1f88d0dc7e08bbab28d4a9b2d9a70ef7ecf
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-04 11:55:21 +0300

    Setting up boolean state variables for user authentication and conditional rendering

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+    const navigate = useNavigate()
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
         <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
@@ -24,8 +25,8 @@ const Navbar = () => {
                 <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
         </ul>
-        <div>
-            <button>Create Account</button>
+        <div className='flex item-center gap-4'>
+            <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light md:block'>Create Account</button>
         </div>
     </div>
   )
