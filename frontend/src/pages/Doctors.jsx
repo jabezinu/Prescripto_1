f8d4caf01d752174c8ff4254e8e5739eadeb39bb
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-05 04:30:28 +0300

    Update Doctors page to include navigation functionality for doctor specialties, enhancing user interactivity and experience.

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -26,12 +26,12 @@ const Doctors = () => {
         <p className='text-gray-600'>Browse through the doctors specialist.</p>
         <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
           <div className='flex flex-col gap-4 text-sm text-gray-600'>
-            <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 round transition-all cursor-pointer`}>General physician</p>
-            <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 round transition-all cursor-pointer`}>Gynecologist</p>
-            <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 round transition-all cursor-pointer`}>Dermatologist</p>
-            <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 round transition-all cursor-pointer`}>pediatricians</p>
-            <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 round transition-all cursor-pointer`}>Neurologist</p>
-            <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 round transition-all cursor-pointer`}>Gastroenterologist</p>
+            <p onClick={() => speciality === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 round transition-all cursor-pointer`}>General physician</p>
+            <p onClick={() => speciality === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 round transition-all cursor-pointer`}>Gynecologist</p>
+            <p onClick={() => speciality === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 round transition-all cursor-pointer`}>Dermatologist</p>
+            <p onClick={() => speciality === 'pediatricians' ? navigate('/doctors') : navigate('/doctors/pediatricians')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 round transition-all cursor-pointer`}>pediatricians</p>
+            <p onClick={() => speciality === 'Neurologist' ? navigate('/doctors') : navigate('/doctors/Neurologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 round transition-all cursor-pointer`}>Neurologist</p>
+            <p onClick={() => speciality === 'Gastroenterologist' ? navigate('/doctors') : navigate('/doctors/Gastroenterologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 round transition-all cursor-pointer`}>Gastroenterologist</p>
           </div>
           <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
             {
