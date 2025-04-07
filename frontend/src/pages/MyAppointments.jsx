e6ae384958744497b8f8dfefed5d85b789367eff
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-07 22:37:29 +0300

    done with the html of myappointment page

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -8,10 +8,27 @@ const MyAppointments = () => {
   return (
     <div>
       <p>My Appointments</p>
+      <hr />
       <div>
         {doctors.slice(0, 2).map((items, index) => (
           <div key = {index}>
+            <div>
+              <img src={items.image} alt="" />
+            </div>
 
+            <div>
+              <p>{items.name}</p>
+              <p>{items.speciality}</p>
+              <p>Address:</p>
+              <p>{items.address.line1}</p>
+              <p>{items.address.line2}</p>
+              <p><span>Date & Time: 25, July, 2024 | 8:30 PM </span></p>
+            </div>
+            <div></div>
+            <div>
+              <button>Pay Online</button>
+              <button>Cancel Appointment</button>
+            </div>
           </div>
         ))}
       </div>
