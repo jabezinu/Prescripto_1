0ed09aba2b34e5bd47e11cf88e47036a9df9933d
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-07 03:16:26 +0300

    Refactor Appointment component to correct time formatting, improve CSS classes for responsiveness, and enhance booking slot display with better layout and functionality for selecting appointment times.

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -47,7 +47,7 @@ const Appointment = () => {
       let timeSlots = []
 
       while(currentDate < endTime) {
-        let formattedTime = currentDate.toLocaleDateString([], {hour: '2-digit', minute: '2-digit'})
+        let formattedTime = currentDate.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
         
         // add slot to array
         timeSlots.push({
@@ -79,24 +79,24 @@ const Appointment = () => {
         {/* -------------------Doctor Details -------------- */}
         <div className='flex flex-col sm:flex-row gap-4'>
           <div>
-            <img className='bg-primary w-full sm:max-2-72 rounded-lg' src={docInfo.image} alt="" />
+            <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
           </div>
           <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
             {/* ---------------- Doc Info: name, degree, experience----------- */}
-            <p className='flex itmes -center gap-2 text-2xl font-medium text-gray-900'>
+            <p className='flex itmes-center gap-2 text-2xl font-medium text-gray-900'>
               {docInfo.name}  
               <img className='w-5' src={assets.verified_icon} alt="" />
             </p>
-            <div className='flex items-center gap-w text-sm mt-1 text-gray-600'>
+            <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
               <p>{docInfo.degree} - {docInfo.speciality}</p>
               <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience}</button>
             </div>
             {/* ----------Doctor About -------------- */}
             <div>
-              <p className='flex items-xenter gap-1 text-sm font-medium text-gray-900 mt-3'>
+              <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>
                 About <img src={assets.info_icon} alt="" />
               </p>
-              <p className='text-sm text-gray-500 max-w-[700px mt-1] '>{docInfo.about}</p>
+              <p className='text-sm text-gray-500 max-w-[700px] mt-1 '>{docInfo.about}</p>
             </div>
             <p className='text-gray-500 font-medium mt-4'>
               Appointment fee: <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span>
@@ -105,18 +105,26 @@ const Appointment = () => {
         </div>
 
         {/* ---------Booking Slots ---------- */}
-        <div className='sm:ml-42 smpl-4 mt-4 font-medium text-gray-700'>
+        <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
           <p>Booking slots</p>
-          <div>
+          <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
             {
               docSlots.length && docSlots.map((item, index) => (
-                <div key={index}>
+                <div onClick={() => setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' : `border border-gray-200`}`} key={index}>
                   <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
                   <p>{item[0] && item[0].datetime.getDate()}</p>
                 </div>
               ))
             }
           </div>
+          <div className='flex item-center gap-3 w-full overflow-x-scroll mt-4'>
+            {docSlots.length && docSlots[slotIndex].map((item, index)=>(
+              <p onClick={() => setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300 '}`} key={index}>
+                {item.time.toLowerCase()}
+              </p>
+            ))}
+          </div>
+          <button className='bg-primary tex-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
         </div>
     </div>
   )
