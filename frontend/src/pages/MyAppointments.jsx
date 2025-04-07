29e3a1be892f93dfded5868baae56e3a583f5f5f
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-07 22:20:18 +0300

    done with myprofile page

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {AppContext} from '../context/AppContext'
 
 const MyAppointments = () => {
+
+  const {doctors} = useContext(AppContext)
+
   return (
     <div>
+      <p>My Appointments</p>
+      <div>
+        {doctors.slice(0, 2).map((items, index) => (
+          <div key = {index}>
+
+          </div>
+        ))}
+      </div>
         
     </div>
   )
