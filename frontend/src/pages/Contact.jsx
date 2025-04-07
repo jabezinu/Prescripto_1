83f0865ebc44f9cd0e7a98be2fafad1897660b75
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-07 05:18:48 +0300

    html of contact us page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Contact = () => {
   return (
-    <div>
-        
+    <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>CONTACT <span className='tezxt-gray-700 font-semibold'>US</span></p>
+        <div>
+          <img src={assets.contact_image} alt="" />
+          <div>
+            <p>OUR OFFICE</p> 
+            <p>54709 Wilms Station <br />Suite 350, Washington, USA</p>
+            <p>Tel: (412) - 1234- 234</p>
+            <p>Email- barberito@gmail.com</p>
+            <p>Careers at BARBARITO</p>
+            <p>Learn more about our teams and job openings.</p>
+            <button>Explore Jobs</button>
+          </div>
+        </div>
+
     </div>
   )
 }
