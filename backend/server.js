54bc253469977662cbbdb7142eaec8d1c5c65953
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-08 04:58:22 +0300

    login controller is done

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors());
 
 // api endpoints
-app.use('/api/', adminRouter)
+app.use('/api/admin', adminRouter)
 // localhost:4000/api/admin/add-doctor
 
 app.get("/", (req, res) => {
