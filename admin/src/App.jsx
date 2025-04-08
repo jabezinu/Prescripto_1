fc24fa0bb639cfa4897e29a15636aa4c9d908530
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-08 05:51:21 +0300

    assetes added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const App = () => {
-  return <div className="text-green-500">hello there</div>;
-};
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-export default App;
+export default App
