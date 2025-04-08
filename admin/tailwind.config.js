af40f408a8f5783694234e52fe081730f244b403
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-09 01:53:03 +0300

    done with login page of admin

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", /* make sure there is no space in here {js,ts,jsx,tsx}*/,
+    "./index.html", "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': '#5F6FFF'
+      }
+    },
   },
   plugins: [],
 };
