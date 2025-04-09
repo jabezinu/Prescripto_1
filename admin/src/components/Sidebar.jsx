36eeae5569d93d807234194225a1609c112e92aa
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-10 00:00:07 +0300

    Add routing for admin pages and update Sidebar to display navigation links based on authentication token

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AdminContext } from '../context/AdminContex'
+import { NavLink } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const Sidebar = () => {
+
+    const {aToken} = useContext(AdminContext)
   return (
     <div>
-        Sidebar
+        {
+            aToken && <ul>
+
+                <NavLink>
+                    <img src={assets.home_icon} alt="" />
+                    <p>Dashbord</p>
+                </NavLink>
+                <NavLink>
+                    <img src={assets.appointment_icon} alt="" />
+                    <p>Appointment</p>
+                </NavLink>
+                <NavLink>
+                    <img src={assets.add_icon} alt="" />
+                    <p>Add Doctor</p>
+                </NavLink>
+                <NavLink>
+                    <img src={assets.people_icon} alt="" />
+                    <p>Doctors List</p>
+                </NavLink>
+            </ul>
+        }
     </div>
   )
 }
