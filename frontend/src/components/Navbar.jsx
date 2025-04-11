320d2e63f9d3151dfed08d5805c665ce3719e3ad
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-11 23:06:31 +0300

    Refactor Navbar component to improve code readability by consolidating profile navigation items

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -50,21 +50,9 @@ const Navbar = () => {
             <img className="w-2.5 " src={assets.dropdown_icon} alt="" />
             <div className="absolute top-0 right-0 pt-14 text-base text-medium text-gray-600 z-20 hidden group-hover:block">
               <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
-                <p
-                  onClick={() => navigate("my-profile")}
-                  className="hover:text-black"
-                >
-                  My Profile
-                </p>
-                <p
-                  onClick={() => navigate("my-appointments")}
-                  className="hover:text-black"
-                >
-                  My Appointment
-                </p>
-                <p onClick={logout} className="hover:text-black">
-                  Logout
-                </p>
+                <p onClick={() => navigate("my-profile")} className="hover:text-black">My Profile</p>
+                <p onClick={() => navigate("my-appointments")} className="hover:text-black" >My Appointment</p>
+                <p onClick={logout} className="hover:text-black">Logout</p>
               </div>
             </div>
           </div>
