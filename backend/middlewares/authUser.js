b41bf4c0b2dc3368e306ef01ab5109118c0709c6
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-12 00:44:24 +0300

    Refactor authUser middleware to simplify token extraction and enhance error handling for authorization

--- backend/middlewares/authUser.js
+++ backend/middlewares/authUser.js
@@ -3,15 +3,14 @@ import jwt from "jsonwebtoken";
 // use authentication middleware
 const authUser = async (req, res, next) => {
   try {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    const { token } = req.headers;
+    if (!token) {
       return res.json({
         success: false,
         message: "not authorized login again",
       });
     }
 
-    const token = authHeader.split(' ')[1];
     const token_decode = jwt.verify(token, process.env.JWT_SECRET);
     if (!req.body) req.body = {};
     req.body.userId = token_decode.id;
