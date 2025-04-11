17d08dbb0091b4e058036987581b73584c64b06e
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-11 23:06:55 +0300

    Refactor Login component to improve code readability by consolidating input field definitions and streamlining API request handling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,25 +19,20 @@ const Login = () => {
 
     try {
       if (state === "Sign Up") {
-        const { data } = await axios.post(backendUrl + "/api/user/register", {
-          name,
-          password,
-          email,
-        });
+        const { data } = await axios.post(backendUrl + "/api/user/register", {name, password, email});
         if (data.success) {
           localStorage.setItem("token", data.token);
           setToken(data.token);
+          navigate("/");
         } else {
           toast.error(data.message);
         }
       } else {
-        const { data } = await axios.post(backendUrl + "/api/user/login", {
-          password,
-          email,
-        });
+        const { data } = await axios.post(backendUrl + "/api/user/login", {password,email});
         if (data.success) {
           localStorage.setItem("token", data.token);
           setToken(data.token);
+          navigate("/");
         } else {
           toast.error(data.message);
         }
@@ -65,63 +60,24 @@ const Login = () => {
         {state === "Sign Up" && (
           <div className="w-full">
             <p>Full Name</p>
-            <input
-              className="border border-zinc-300 rounded w-full p-2 mt-1"
-              type="text"
-              onChange={(e) => setName(e.target.value)}
-              value={name}
-              required
-            />
+            <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="text" onChange={(e) => setName(e.target.value)} value={name} required/>
           </div>
         )}
 
         <div className="w-full">
           <p>Email</p>
-          <input
-            className="border border-zinc-300 rounded w-full p-2 mt-1"
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            required
-          />
+          <input className="border border-zinc-300 rounded w-full p-2 mt-1"type="email" onChange={(e) => setEmail(e.target.value)} value={email} required />
         </div>
         <div className="w-full">
           <p>Password</p>
-          <input
-            className="border border-zinc-300 rounded w-full p-2 mt-1"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            required
-          />
+          <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="password" onChange={(e) => setPassword(e.target.value)} value={password} required />
         </div>
-        <button
-          type="submit"
-          className="bg-primary text-white w-full py-2 rounded-md text-base"
-        >
-          {state === "Sign Up" ? "Create Account" : "Login"}
-        </button>
-        {state === "Sign Up" ? (
-          <p>
-            Already have an account?{" "}
-            <span
-              onClick={() => setState("Login")}
-              className="text-primary underline cursor-pointer"
-            >
-              Login here
-            </span>
-          </p>
-        ) : (
-          <p>
-            Create a new account!{" "}
-            <span
-              onClick={() => setState("Sign Up")}
-              className="text-primary underline cursor-pointer"
-            >
-              click here
-            </span>
-          </p>
-        )}
+        <button type="submit" className="bg-primary text-white w-full py-2 rounded-md text-base">{state === "Sign Up" ? "Create Account": "Login"}</button>
+        {
+          state === "Sign Up" 
+          ? <p>Already have an account?{" "} <span onClick={() => setState("Login")} className="text-primary underline cursor-pointer" >Login here</span></p>
+          : <p>Create a new account!{" "}<span onClick={() => setState("Sign Up")} className="text-primary underline cursor-pointer">click here</span></p>
+        }
       </div>
     </form>
   );
