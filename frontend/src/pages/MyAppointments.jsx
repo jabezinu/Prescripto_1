1fe54de473c3f0b8e27d8a5592dc0d12324d3f7c
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-14 00:29:30 +0300

    Update MyAppointments component to fetch doctors data after user appointment retrieval and conditionally render buttons based on appointment status

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify'
 
 const MyAppointments = () => {
 
-  const {backendUrl, token} = useContext(AppContext)
+  const {backendUrl, token, getDoctorsData} = useContext(AppContext)
 
   const [appointments, setAppointments] = useState([])
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -42,6 +42,7 @@ const MyAppointments = () => {
       if(data.success){
         toast.success(data.message)
         getUserAppointment()
+        getDoctorsData()
       }else{
         toast.error(data.message)
       }
@@ -79,8 +80,9 @@ const MyAppointments = () => {
             </div>
             <div></div>
             <div className='flex flex-col gap-2 justify-end'>
-              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>
-              <button onClick={() => cancelAppointment(items._id)}className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>
+              {!items.cancelled && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
+              {!items.cancelled && <button onClick={() => cancelAppointment(items._id)}className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
+              {items.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment cancelled</button>}
             </div>
           </div>
         ))}
