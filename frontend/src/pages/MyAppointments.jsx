80e3114e4cb44f560318b1b69bd8c1e60a3fc23d
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-13 22:12:29 +0300

    Import toast notification for user feedback in MyAppointments component

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import {AppContext} from '../context/AppContext'
 import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const MyAppointments = () => {
 
