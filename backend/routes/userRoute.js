3e6d31948e25068d27fac5d62ac7075830b551ba
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-13 23:45:37 +0300

    Add cancel appointment route to userRouter

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   bookAppointment,
+  cancelAppointment,
   getProfile,
   listAppointment,
   loginUser,
@@ -14,10 +15,12 @@ const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+
 userRouter.get("/get-profile", authUser, getProfile);
 userRouter.post("/update-profile", upload.single('image'), authUser, updateProfile);
 userRouter.post("/book-appointment", authUser, bookAppointment);
 userRouter.get("/appointments", authUser, listAppointment);
+userRouter.post("/cancel-appointment", authUser, cancelAppointment);
 
 
 export default userRouter;
