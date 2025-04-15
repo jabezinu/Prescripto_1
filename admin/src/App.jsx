89c8ec2312758e043350edbf11af11901fb37296
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-15 03:46:48 +0300

    Integrate DoctorContext into App component, adding new routes for DoctorDashboard, DoctorAppointments, and DoctorProfile. Create DoctorProfile component for future implementation, enhancing the overall structure for doctor-related functionalities.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,21 +10,32 @@ import AllAppointments from "./pages/Admin/AllAppointments";
 import Dashboard from "./pages/Admin/Dashboard";
 import AddDoctor from "./pages/Admin/AddDoctor";
 import DoctorsList from "./pages/Admin/DoctorsList";
+import { DoctorContext } from "./context/DoctorContext";
+import DoctorDashboard from "./pages/Doctor/DoctorDashboard";
+import DoctorAppointments from "./pages/Doctor/DoctorAppointments";
+import DoctorProfile from "./pages/Doctor/DoctorProfile";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
-  return aToken ? (
+  const { dToken } = useContext(DoctorContext);
+  return aToken || dToken ? (
     <div className="bg-[#F8F9FD">
       <ToastContainer />
       <Navbar />
       <div className="flex items-start">
         <Sidebar />
         <Routes>
+          {/* Admin Routes */}
           <Route path="/" element={<></>}/>
           <Route path="/admin-dashboard" element={<Dashboard/>}/>
           <Route path="/all-appointment" element={<AllAppointments/>}/>
           <Route path="/add-doctor" element={<AddDoctor/>}/>
           <Route path="/doctors-list" element={<DoctorsList/>}/>
+
+          {/* Doctor Routes */}
+          <Route path="/doctor-dashboard" element={<DoctorDashboard/>}/>
+          <Route path="/doctor-appointment" element={<DoctorAppointments/>}/>
+          <Route path="/doctor-profile" element={<DoctorProfile/>}/>
         </Routes>
       </div>
     </div>
