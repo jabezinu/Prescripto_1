0b87dfbe3b17075b775bdcef60a0f0073967914e
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-15 03:47:02 +0300

    Enhance Sidebar component to include DoctorContext, adding new navigation links for doctor-specific routes and updating the display logic for admin and doctor users. Implement dashboard data retrieval in AdminContext for improved functionality.

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -2,9 +2,11 @@ import React, { useContext } from "react";
 import { AdminContext } from "../context/AdminContex";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
+import { DoctorContext } from "../context/DoctorContext";
 
 const Sidebar = () => {
   const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext);
   return (
     <div className="min-h-screen bg-white border-r">
       {aToken && (
@@ -29,7 +31,7 @@ const Sidebar = () => {
             to={"/all-appointment"}
           >
             <img src={assets.appointment_icon} alt="" />
-            <p>Appointment</p>
+            <p className="hidden md:block">Appointment</p>
           </NavLink>
           <NavLink
             className={({ isActive }) =>
@@ -40,7 +42,7 @@ const Sidebar = () => {
             to={"/add-doctor"}
           >
             <img src={assets.add_icon} alt="" />
-            <p>Add Doctor</p>
+            <p className="hidden md:block">Add Doctor</p>
           </NavLink>
           <NavLink
             className={({ isActive }) =>
@@ -51,10 +53,31 @@ const Sidebar = () => {
             to={"/doctors-list"}
           >
             <img src={assets.people_icon} alt="" />
-            <p>Doctors List</p>
+            <p className="hidden md:block">Doctors List</p>
           </NavLink>
         </ul>
-      )}
+      )
+      }
+
+{dToken && (
+        <ul className="text-[#515151] mt-5">
+          <NavLink
+            className={({ isActive }) =>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-2-72 cursor-pointer ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`}to={"/doctor-dashboard"}>
+            <img src={assets.home_icon} alt="" />
+            <p className="hidden md:block">Dashbord</p>
+          </NavLink>
+          <NavLink className={({ isActive }) =>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-2-72 cursor-pointer ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`}to={"/doctor-appointment"}>
+            <img src={assets.appointment_icon} alt="" />
+            <p className="hidden md:block">Appointment</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-2-72 cursor-pointer ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`}to={"/doctor-profile"}>
+            <img src={assets.people_icon} alt="" />
+            <p className="hidden md:block">Profile</p>
+          </NavLink>
+        </ul>
+      )
+      }
     </div>
   );
 };
--- admin/src/context/AdminContex.jsx
+++ admin/src/context/AdminContex.jsx
@@ -8,6 +8,7 @@ const AdminContextProvider = (props) => {
   const [aToken, setAToken] = useState(localStorage.getItem("aToken") ? localStorage.getItem("aToken") : "");
   const [doctors, setDoctors] = useState([]);
   const [appointments, setAppointments] = useState([])
+  const [dashData, setDashData] = useState(false)
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
@@ -76,6 +77,21 @@ const AdminContextProvider = (props) => {
     }
   }
 
+  const getDashData = async () => {
+    try {
+      
+      const {data} = await axios.get(backendUrl + "/api/admin/dashboard", {headers: {atoken: aToken}})
+      if(data.success){
+        setDashData(data.dashData)
+        console.log(data.dashData);        
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)      
+    }
+  }
+
   const value = {
     aToken,
     setAToken,
@@ -86,6 +102,7 @@ const AdminContextProvider = (props) => {
     appointments, setAppointments,
     getAllAppointments,
     cancelAppointment,
+    dashData, getDashData
   };
 
   return (
