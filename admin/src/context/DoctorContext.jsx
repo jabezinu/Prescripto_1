ba231c1dea14f61944912977a0db1bb9192c66b1
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-17 00:23:14 +0300

    Add dashboard data retrieval to DoctorContext and introduce Axios helper functions
    - Added state management for dashboard data in DoctorContext.
    - Implemented `getDashData` function to fetch dashboard data from the API.
    - Introduced `isAbsoluteURL` and `isAxiosError` helper functions for improved URL handling and error detection in Axios.

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -9,6 +9,7 @@ const DoctorContextProvider = (props) => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [dToken, setDToken] = useState(localStorage.getItem("dToken") ? localStorage.getItem("dToken") : "");
   const [appointments, setAppointments] = useState([])
+  const [dashData, setDashData] = useState(false);
 
   const getAppointments = async() => {
     try {
@@ -57,13 +58,33 @@ const DoctorContextProvider = (props) => {
       
     }
   }
+  
+  const getDashData = async () => {
+    try {
+      const {data} = await axios.get(backendUrl + '/api/docotr/dashboard', {headers: {dtoken: dToken}})
+      
+      if(data.success){
+        setDashData(data.dashData)
+        console.log(data.dashData);
+        
+      }else{
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)      
+    }
+  }
+
   const value = {
     dToken, setDToken,
     backendUrl,
     appointments, setAppointments,
     getAppointments,
     completeAppointment,
-    cancelAppointment
+    cancelAppointment,
+    dashData, setDashData, getDashData
   }
 
   return (
