f906a9244756c921c7867fe4f181bddb788db1bd
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-17 22:23:51 +0300

    Enhance DoctorProfile component with editable fields and state management
    - Added state management for edit mode in the DoctorProfile component.
    - Implemented editable input fields for appointment fee and address, allowing users to modify profile data directly.
    - Updated checkbox to reflect the doctor's availability status.
    - Enhanced the Edit button functionality to toggle the edit mode.

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { DoctorContext } from '../../context/DoctorContext'
 import { AppContext } from '../../context/AppContext';
 
@@ -6,6 +6,8 @@ const DoctorProfile = () => {
 
   const {dToken, profileData, setProfileData, getProfileData} = useContext(DoctorContext);
   const {currency, backendUrl} = useContext(AppContext);
+
+  const [isEdit, setIsEdit] = useState(false)
 
   useEffect(() => {
     if(dToken){
@@ -38,24 +40,25 @@ const DoctorProfile = () => {
           </div>
 
           <p className='text-gray-600 font-medium mt-4'>
-            Appointment fee: <span className='text-gray-800'>{currency} {profileData.fees}</span>
+            Appointment fee: <span className='text-gray-800'>{currency} {isEdit ? <input type="number" onChange={(e) => setProfileData(prev => ({...prev, fees: e.target.value}))} value={profileData.fees} /> : profileData.fees}</span>
+          
           </p>
 
           <div className='flex gap-2 py-2'>
             <p>Address: </p>
             <p className='text-sm'>
-              {profileData.address.line1}
+              {isEdit ? <input type="text" onChange={(e) => setProfileData(prev => ({...prev, address: {...prev.address, line1: e.target.value}}))} value={profileData.address.line1}/> : profileData.address.line1}
               <br />
               {profileData.address.line2}
             </p>
           </div>
 
           <div className='flex gap-1 pt-2'>
-            <input type="checkbox" />
+            <input checked ={profileData.available} type="checkbox" />
             <label htmlFor="">Available</label>
           </div>
 
-          <button className='px-4 py-1 border border-primary text-sm rounded-full mt-5  hover:bg-primary hover:text-white transition-all'>Edit</button>
+          <button onClick={() =>setIsEdit(true)} className='px-4 py-1 border border-primary text-sm rounded-full mt-5  hover:bg-primary hover:text-white transition-all'>Edit</button>
         </div>
       </div>
         
