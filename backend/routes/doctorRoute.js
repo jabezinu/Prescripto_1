0bcd2389f0c9ce6f98fbbebb9e92d517c14129ae
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-19 19:50:57 +0300

    Add new doctor-related endpoints for appointment management and profile updates
    - Introduced multiple routes in `doctorRoute.js` for handling doctor appointments, including `appointments`, `complete-appointment`, and `cancel-appointment`.
    - Added authentication middleware for secure access to the new endpoints.
    - Enhanced doctor profile management with routes for `dashboard`, `profile`, and `update-profile`, improving overall functionality for doctors.

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,8 +1,15 @@
 import express from 'express'
-import {doctorList, loginDoctor } from '../controllers/doctorController.js'
+import {appointmentCancel, appointmentComplete, appointmentsDoctor, doctorDashboard, doctorList, doctorProfile, loginDoctor, updateDoctorProfile } from '../controllers/doctorController.js'
+import authDocotr from '../middlewares/authDoctor.js';
 const doctorRouter = express.Router();
 
 doctorRouter.get('/list', doctorList)
 doctorRouter.post('/login', loginDoctor)
+doctorRouter.get('/appointments', authDocotr,appointmentsDoctor)
+doctorRouter.post('/complete-appointment', authDocotr,appointmentComplete)
+doctorRouter.post('/cancel-appointment', authDocotr,appointmentCancel)
+doctorRouter.get('/dashboard', authDocotr,doctorDashboard)
+doctorRouter.get('/profile', authDocotr,doctorProfile)
+doctorRouter.post('/update-profile', authDocotr, updateDoctorProfile)
 
 export default doctorRouter
