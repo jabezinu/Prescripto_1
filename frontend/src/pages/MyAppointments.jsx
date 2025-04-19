c747c47bfb0e072a6fd9f5bfb3ddc3819c3f3405
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-19 19:51:54 +0300

    Update appointment buttons in MyAppointments component
    - Enhanced the `MyAppointments.jsx` component to include conditional rendering for appointment status buttons.
    - Added a "Completed" button for completed appointments and adjusted the visibility of "Pay Online" and "Cancel Appointment" buttons based on both cancellation and completion status, improving user experience.

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -80,9 +80,10 @@ const MyAppointments = () => {
             </div>
             <div></div>
             <div className='flex flex-col gap-2 justify-end'>
-              {!items.cancelled && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
-              {!items.cancelled && <button onClick={() => cancelAppointment(items._id)}className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
-              {items.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment cancelled</button>}
+              {!items.cancelled && !items.isCompleted && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
+              {!items.cancelled && !items.isCompleted && <button onClick={() => cancelAppointment(items._id)}className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
+              {items.cancelled && !items.isCompleted && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment cancelled</button>}
+              {items.isCompleted && <button className='sm:min-w-48 py-2 border border-green-500 rounded text-green-500'>Completed</button>}
             </div>
           </div>
         ))}
